bc840d5431c0876f97c74d4bcd5854f9b21c83e9
liamartinez <sushionthego@gmail.com>
2012-10-17 22:00:20 -0400

    first commit

---
+++ Oct18/Oct18/Oct18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct18AppDelegate.h
+//  Oct18
+//
+//  Created by Lia Martinez on 10/17/12.
+//  Copyright (c) 2012 liamartinez.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
